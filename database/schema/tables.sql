917596b473e1dfe33ff1b933282ba59ac5da2e2d
MariamAsall <mariamasal075@gmail.com>
2026-04-03 23:18:38 +0200

    created Instructor - Student - InstructorCourse - StudentTrack Tables

---
+++ database/schema/tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Instructor(
+	InstructorID SERIAL PRIMARY KEY ,
+	Name TEXT,
+	Email TEXT UNIQUE,
+	DepartmentNo INT REFERENCES Departments(DepartmentID) ON DELETE RESTRICT
+);
+
+CREATE TABLE Student (
+	StudentID SERIAL PRIMARY KEY,
+	Name TEXT,
+	Email TEXT UNIQUE,
+	Phone TEXT
+);
+
+CREATE TABLE InstructorCourse (
+	InstructorID INT REFERENCES Instructor(InstructorID),
+	CourseID INT REFERENCES Course(CourseID),
+	PRIMARY KEY (InstructorID,CourseID)
+);
+
+CREATE TABLE StudentTrack(
+ 	StudentID INT REFERENCES Student(StudentID) ON DELETE CASCADE,
+	TrackID INT REFERENCES Track(TrackID) ON DELETE CASCADE
+);
