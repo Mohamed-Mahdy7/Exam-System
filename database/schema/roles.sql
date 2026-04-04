20df7ffd9a0d60c1ebb1270f8615d1bb63afaf36
MariamAsall <mariamasal075@gmail.com>
2026-04-04 23:07:30 +0200

    updated instructorcourse , studenttrack fk

--- database/schema/roles.sql
+++ database/schema/roles.sql
@@ -23,3 +23,20 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON TABLES TO admin_role;
 
+
+CREATE ROLE instructor_role WITH LOGIN PASSWORD 'instructor123';
+CREATE ROLE student_role WITH LOGIN PASSWORD 'student123';
+
+GRANT SELECT , INSERT ,UPDATE ,DELETE ON Instructor TO instructor_role;
+GRANT SELECT ON Instructor TO student_role;
+
+GRANT SELECT , INSERT ,UPDATE ,DELETE ON Student TO instructor_role;
+GRANT SELECT ON Student TO student_role;
+
+GRANT SELECT ,INSERT ,UPDATE ,DELETE ON StudentTrack TO instructor_role;
+GRANT SELECT ON StudentTrack TO student_role;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON InstructorCourse TO instructor_role;
+REVOKE ALL ON InstructorCourse FROM student_role;
+
+REVOKE INSERT , UPDATE, DELETE ON Student FROM student_role;
