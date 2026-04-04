f03fe6b439d7954121da2e10ac5ec39626e3702d
sami <ahmedsamy121277@gmai.com>
2026-04-05 00:04:52 +0200

    rerequest

--- database/schema/indexes.sql
+++ database/schema/indexes.sql
@@ -1,3 +1,5 @@
 create index idx_track_department on Track (DepartmentID);
 create index idx_trackcourse_track on TrackCourse (TrackID);
 create index idx_trackcourse_course on TrackCourse (CourseID);
+
+
