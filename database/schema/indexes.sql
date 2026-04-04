9f89c14d3a312f9140199664751201138571512f
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-04-05 00:12:26 +0200

    remove spaces

--- database/schema/indexes.sql
+++ database/schema/indexes.sql
@@ -1,15 +1,16 @@
 -- Indexes for the QUESTIONS and CHOICE tables
 CREATE INDEX idx_questions_course ON Questions(CourseID);
 CREATE INDEX idx_choice_question ON Choice(QuestionID);
+
 create index idx_track_department on Track (DepartmentID);
 create index idx_trackcourse_track on TrackCourse (TrackID);
 create index idx_trackcourse_course on TrackCourse (CourseID);
-
 
 create index idx_instructor_department on Instructor(DepartmentNO);
 CREATE INDEX idx_studentTrack_student ON StudentTrack(StudentID);
 CREATE INDEX idx_studentTrack_track ON StudentTrack(TrackID);
 CREATE INDEX idx_instructorCourse_course ON InstructorCourse(CourseID);
+
 CREATE index idx_exam_couse ON Exam(CourseID);
 CREATE index idx_examquestion_exam ON ExamQuestion(ExamID);
 CREATE index idx_examquestion_question ON ExamQuestion(QuestionID);
