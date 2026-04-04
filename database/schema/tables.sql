2e2c54ae64ab95a581578f807412a131ccb64292
sami <ahmedsamy121277@gmai.com>
2026-04-04 19:13:48 +0200

    add constraints and index on fk

--- database/schema/tables.sql
+++ database/schema/tables.sql
@@ -1,26 +1,37 @@
 create table Departments (
 DepartmentID serial primary key ,
-DepartmentName text,
-dep_location text
+DepartmentName text not null,
+location text
 );
+
 
 create table Track (
 TrackID serial primary key ,
-TrackName text,
-DepartmentID int references Departments(DepartmentID) on delete cascade
+TrackName text not null,
+DepartmentID int not null references Departments(DepartmentID) on delete restrict
 
 );
 
 
 create table Course(
 CourseID serial primary key,
-CourseName  text,
-MinDegree int ,
-MaxDegree int
+CourseName  text not null,
+MinDegree int not null,
+MaxDegree int not null ,
+CHECK (MinDegree >= 0),
+CHECK (MaxDegree > MinDegree)
 );
 
 create table TrackCourse(
-TrackID int references Track(TrackID) on delete cascade,
-CourseID int references Course(CourseID) on delete cascade,
+TrackID int not null references Track(TrackID) on delete cascade,
+CourseID int not null references Course(CourseID) on delete cascade,
 primary key (TrackID ,CourseID )
 );
+
+
+
+create index idx_track_department on Track (DepartmentID);
+
+create index idx_trackcourse_track on TrackCourse (TrackID);
+
+create index idx_trackcourse_course on TrackCourse (CourseID);
