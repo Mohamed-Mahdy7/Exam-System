74a296d6302db226d0b910fa1fe9888190b48865
ahmedsamv <ahmedsamy121277@gmail.com>
2026-04-07 16:13:55 +0200

    add crud procedure to departments , track,course and trackcourse and try handle select

---
+++ database/procedures/Course_crud.sql
@@ -0,0 +1,48 @@
+----------------------- insert ----------------------------
+
+create or replace procedure InsertCourses(
+    p_CourseName text,
+	p_MinDegree int , 
+  	p_MaxDegree int 
+) LANGUAGE plpgsql
+AS $$
+BEGIN 
+    INSERT INTO Course (CourseName,MinDegree,MaxDegree ) VALUES(p_CourseName,p_MinDegree,p_MaxDegree );
+ END   
+$$
+
+call InsertCourses ('python' , 40, 100 )
+
+
+----------------------- update ----------------------------
+create or replace procedure UpdateCourses(
+    p_CourseID int,
+    p_CourseName text,
+	p_MinDegree int , 
+  	p_MaxDegree int 
+) LANGUAGE plpgsql
+AS $$
+BEGIN 
+      UPDATE Course
+    SET CourseName = p_CourseName,
+		MinDegree = p_MinDegree,
+        MaxDegree = p_MaxDegree
+    WHERE CourseID = p_CourseID;
+ END   
+$$
+call UpdateCourses ( 5 , 'Java', 60, 100)
+----------------------- delete  ----------------------------
+CREATE OR REPLACE PROCEDURE DeleteCourse(
+     p_CourseID int
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM Course
+    WHERE CourseID = p_CourseID;
+END;
+$$;
+
+call DeleteCourse(4 )
+
+select * from Course ;
